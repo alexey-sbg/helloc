1e8538b24ea3f10d0ed7c866c818a04ed2f23bdd
Alexey Dolgoborodov <alexey.dolgoborodov@sbgenomics.com>
2016-03-04 12:22:29 +0000

    Added file I/O to the project

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 This is a super small c example where a single file gets compiled on the 
 Docker image and then imported on the platform.
 
+The application will act as a filter for lines starting with ## if input and output files are specified.
 
 Check the `Dockerfile` to see how to compile a C program and add it to the path.
--- hello.c
+++ hello.c
@@ -1,9 +1,67 @@
+/**
+ * This program prints "Hello, World!" and acts as a filter extracting meta-information lines from VCF file.
+ * It is safe to run on any other text file, though result will probably be less impressing.
+ *
+ * (c) Seven Bridge Genomics, 2016
+ */
+
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int main(int argc, char * argv[])
 {
-  printf ("Hello world\n");
-  return 0;
+    static char buf[4096];
+
+    FILE * f_input;
+    FILE * f_output;
+    unsigned long long count;
+    int is_new_line;
+
+    printf("Hello, World!\n");
+
+    if (argc != 3) {
+        printf("\nIf you would specify input and output file, this program "
+                       "would filter for all lines starting with ##,\n"
+                       "Usage:\n\t$ helloc <input_file> <output_file>\n");
+        return 0;
+    }
+
+    printf("\nFiltering meta-information...\n\t%s ==> %s\n", argv[1], argv[2]);
+
+    f_input = fopen(argv[1], "r");
+    if (f_input == NULL)
+    {
+        printf("\nOops! Failed to open file %s for reading. Bye!\n", argv[1]);
+        return 1;
+    }
+
+    f_output = fopen(argv[2], "w");
+    if (f_output == NULL)
+    {
+        printf("\nOops! Failed to open file %s for writing. Bye!\n", argv[2]);
+        return 2;
+    }
+
+    count = 0;
+    is_new_line = 1;
+
+    while (1) {
+        char * p = fgets(buf, 4096, f_input);
+        size_t n;
+        if ((p == NULL) || ((n = strlen(buf)) == 0)) break;
+
+        if (is_new_line && (buf[0] == '#') && (buf[1] == '#')) {
+            fputs(buf, f_output);
+            count++;
+        }
+
+        is_new_line = (buf[n - 1] == '\n');
+    }
+
+    fclose(f_input);
+    fclose(f_output);
+
+    printf("\nFound %llu lines starting with '##', filtered it all to output file.\n"
+                   "\nChiao!\n", count);
+    return 0;
 }
-
-
